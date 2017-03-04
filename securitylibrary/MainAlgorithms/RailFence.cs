b2798368ab7aada47a1bca62977264d4f1cb525c
Khaled sherif <khaledkeky@gmail.com>
2017-03-04 16:03:41 +0200

    RailFenceDecryption

--- securitylibrary/MainAlgorithms/RailFence.cs
+++ securitylibrary/MainAlgorithms/RailFence.cs
@@ -15,7 +15,39 @@ namespace SecurityLibrary
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            double length = cipherText.Length;
+            double coldouble = length / key;
+            int col_num = (int)(coldouble + 0.5);
+            string plainText = "";
+            int counter = 0;
+            char[,] table = new char[key, col_num];
+            for (int i = 0; i < key; i++)
+            {
+                for (int j = 0; j < col_num; j++)
+                {
+                    if (counter >= length)
+                    {
+                        break;
+                    }
+                    table[i,j] = cipherText[counter];
+                    counter++;
+                }
+            }
+            for (int j = 0; j < col_num; j++)
+            {
+                for (int i = 0; i < key; i++)
+                {
+                    if ((j + 1) * (i + 1) > length)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        plainText += table[i, j];
+                    }
+                }
+            }
+            return plainText.ToLower() ;
         }
 
         public string Encrypt(string plainText, int key)
