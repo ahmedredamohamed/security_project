f202a013ff08e2eff1768471f2bfd074078c1faa
Khaled sherif <khaledkeky@gmail.com>
2017-03-04 20:30:03 +0200

    Columnar Encryption And Decryption

--- securitylibrary/MainAlgorithms/Columnar.cs
+++ securitylibrary/MainAlgorithms/Columnar.cs
@@ -15,12 +15,91 @@ namespace SecurityLibrary
 
         public string Decrypt(string cipherText, List<int> key)
         {
-            throw new NotImplementedException();
+            int bigColNum = key.Max();
+            double length = cipherText.Length;
+            double rowdouble = length / bigColNum;
+            int row_num = (int)Math.Ceiling(rowdouble);
+            string plainText = "";
+            int counter = 0;
+            char[,] table = new char[row_num, bigColNum];
+            char[,] DecrubtedTable = new char[row_num, bigColNum];
+            for (int j = 0; j < bigColNum; j++)
+            {
+                for (int i = 0; i < row_num; i++)
+                {
+                    if (counter >= length)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        table[i, j] = cipherText[counter];
+                        counter++;
+                    }
+                }
+            }
+ 
+           
+            for (int i = 0; i < key.Count; i++)
+            {
+                for (int j = 0; j < row_num; j++)
+                {
+                    DecrubtedTable[j, i] = table[j, key.ElementAt(i) - 1];
+                }
+            }
+            for (int i = 0; i < row_num; i++)
+            {
+                for (int j = 0; j < bigColNum; j++)
+                {
+                    if (DecrubtedTable[i, j] == 'x')
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        plainText += DecrubtedTable[i, j];
+                    }
+                }
+            }
+            return plainText.ToLower();
         }
 
         public string Encrypt(string plainText, List<int> key)
         {
-            throw new NotImplementedException();
+            int bigColNum = key.Max();
+            double length = plainText.Length;
+            double rowdouble = length / bigColNum;
+            int row_num = (int)Math.Ceiling(rowdouble);
+            string cipherText = "";
+            int counter = 0;
+            char[,] table = new char[row_num, bigColNum];
+            Dictionary<int, int> dictionary = new Dictionary<int,int>();
+            for (int i = 0; i < row_num; i++)
+            {
+                for (int j = 0; j < bigColNum; j++)
+                {
+                    if (counter >= length)
+                    {
+                        table[i, j] = 'x';
+                    }
+                    else
+                    {
+                        table[i, j] = plainText[counter];
+                        counter++;
+                    }
+                }
+            }
+            for (int i = 0; i < key.Count; i++) {
+                dictionary.Add(key.ElementAt(i),i);
+            }
+            for (int i = 0; i < key.Count; i++) {
+                int index;
+                dictionary.TryGetValue(i + 1, out index);
+                for (int j = 0; j < row_num; j++) {
+                    cipherText += table[j, index];
+                }
+            }
+            return cipherText.ToUpper();
         }
     }
 }
