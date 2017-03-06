9cd51dc21caf26c6fba8bd9eedc976feb43908f8
Khaled sherif <khaledkeky@gmail.com>
2017-03-06 17:48:09 +0200

    Columnar Successfully Completed

--- securitylibrary/MainAlgorithms/Columnar.cs
+++ securitylibrary/MainAlgorithms/Columnar.cs
@@ -10,7 +10,91 @@ namespace SecurityLibrary
     {
         public List<int> Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            int row_num = 2;
+            string cipher = cipherText.ToLower();
+            double length = plainText.Length;
+            double cipherlength = cipher.Length;
+            List<int> Key = new List<int>();
+            for (int k = 0; k <= plainText.Length; k++)
+            {
+                double rowdouble = length / row_num;
+                int col_num = (int)Math.Ceiling(rowdouble);
+                char[,] tablePlain = new char[row_num, col_num];
+                char[,] tableCipher = new char[row_num, col_num];
+                int counterPlain = 0;
+                int counterCipher = 0;
+                string[] Textplain = new string[col_num];
+                string[] CipherText = new string[col_num];
+                for (int i = 0; i < row_num; i++)
+                {
+                    for (int j = 0; j < col_num; j++)
+                    {
+                        if (counterPlain >= length)
+                        {
+                            tablePlain[i, j] = 'x';
+                        }
+                        else
+                        {
+                            tablePlain[i, j] = plainText[counterPlain];
+                            counterPlain++;
+                        }
+                    }
+                }
+                for (int j = 0; j < col_num; j++)
+                {
+                    for (int i = 0; i < row_num; i++)
+                    {
+                        if (counterCipher >= cipherlength)
+                        {
+                            tableCipher[i, j] = 'x';
+                        }
+                        else
+                        {
+                            tableCipher[i, j] = cipher[counterCipher];
+                            counterCipher++;
+                        }
+                    }
+                }
+                    for (int i = 0; i < col_num; i++)
+                    {
+                        for (int j = 0; j < row_num; j++)
+                        {
+                            CipherText[i] += tableCipher[j, i];
+                            Textplain[i] += tablePlain[j, i];
+                        }
+                }
+                int counterMatching = 0;
+                for (int i = 0; i < CipherText.Length; i++) {
+                    for (int j = 0; j < Textplain.Length; j++) {
+                        if (CipherText[i] == Textplain[j])
+                        {
+                            counterMatching++;
+                        }
+                    }
+                }
+                if (counterMatching == col_num)
+                {
+                    for(int i = 0; i< Textplain.Length; i++)
+                    {
+                        for (int j = 0; j < CipherText.Length; j++) {
+                            if (Textplain[i] == CipherText[j]) {
+                                Key.Insert(i, j + 1);
+                                j = CipherText.Length;
+                            }
+                        }
+                    }
+                    break;
+                }
+                else {
+                    row_num++; 
+                }
+            }
+            if (Key.Count == 0) {
+                for (int i = 0; i < 100; i++) {
+                    Key.Add(0);
+                }
+            }
+            return Key;
         }
 
         public string Decrypt(string cipherText, List<int> key)
