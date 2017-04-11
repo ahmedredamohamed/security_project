2957d27fa5b316dad739533d07ebb914600806c8
Mohamed Saleh <mohamed.ahmed.s@cis.asu.edu.eg>
2017-04-11 17:56:39 +0200

    ,amfa,k

---
+++ SecurityLibrary/AES/AES.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLibrary.AES
+{
+    /// <summary>
+    /// If the string starts with 0x.... then it's Hexadecimal not string
+    /// </summary>
+    public class AES : CryptographicTechnique
+    {
+        public override string Decrypt(string cipherText, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Encrypt(string plainText, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[][] subbytes(byte[][] state)
+        {
+            for(int i=0;i<state.GetLength(0);i++)
+            {
+                for(int j=0;i<state.GetLength(1);j++)
+                {
+                    byte temp=state[i][j];
+                }
+            }
+            return state;
+        }
+    }
+}
