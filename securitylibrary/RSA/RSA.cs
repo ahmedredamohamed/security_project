2176f882c048d80d727516bda52e244280df62da
Khaled sherif <khaledkeky@gmail.com>
2017-04-22 16:05:34 +0200

    Rsa Encryption

--- securitylibrary/RSA/RSA.cs
+++ securitylibrary/RSA/RSA.cs
@@ -10,7 +10,14 @@ namespace SecurityLibrary.RSA
     {
         public int Encrypt(int p, int q, int M, int e)
         {
-            throw new NotImplementedException();
+            int n = p * q ;
+            int result = 1;
+            for (int i = 0; i < e; i++) {
+                result *= (int)(Math.Pow(M, 1) % n);
+                while (result > n)
+                    result %= n;
+            }
+            return result;
         }
 
         public int Decrypt(int p, int q, int C, int e)
